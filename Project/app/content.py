cc8fa94f760bbbb1f11f695b7b712a699764cd65
Katz Chaim <chaim2025@gmail.com>
2025-09-29 13:41:07 -0400

    new file

--- Project/app/content.py
+++ Project/app/content.py
@@ -1,3 +1,23 @@
-VIDEOS = [{"title":"Intro to CI/CD","embed":"https://www.youtube.com/embed/scEDHsr3APg"}]
-TOOLS  = [{"name":"GitHub Actions","desc":"CI/CD","url":"https://docs.github.com/actions"}]
-LEARNING=[{"title":"Accelerate","desc":"DORA metrics","url":"https://itrevolution.com/accelerate/"}]
+VIDEOS = [
+    {"title": "Intro to CI/CD", "embed": "https://www.youtube.com/embed/scEDHsr3APg"},
+    {"title": "Kubernetes in 15 minutes", "embed": "https://www.youtube.com/embed/s_o8dwzRlu4"},
+    {"title": "Infrastructure as Code (Terraform)", "embed": "https://www.youtube.com/embed/h970ZBgKINg"}
+]
+
+TOOLS = [
+    {"name": "GitHub Actions", "desc": "Multi-branch CI/CD", "url": "https://docs.github.com/actions"},
+    {"name": "Docker", "desc": "Containers & images", "url": "https://docs.docker.com/"},
+    {"name": "Kubernetes", "desc": "Orchestrate at scale", "url": "https://kubernetes.io/docs/"},
+    {"name": "Helm", "desc": "K8s package manager", "url": "https://helm.sh/docs/"},
+    {"name": "Terraform", "desc": "Infrastructure as Code", "url": "https://developer.hashicorp.com/terraform"},
+    {"name": "Prometheus", "desc": "Metrics & alerting", "url": "https://prometheus.io/docs/introduction/overview/"},
+    {"name": "Grafana", "desc": "Dashboards & viz", "url": "https://grafana.com/docs/"}
+]
+
+LEARNING = [
+    {"title": "Accelerate (Book)", "desc": "The DORA research behind high-perf teams.", "url": "https://itrevolution.com/accelerate/"},
+    {"title": "Google SRE Book", "desc": "SLI/SLO, error budgets, release policies.", "url": "https://sre.google/books/"},
+    {"title": "Docker Getting Started", "desc": "Hands-on walkthroughs.", "url": "https://docs.docker.com/get-started/"},
+    {"title": "Kubernetes Docs – Learn", "desc": "Concepts, tasks, tutorials.", "url": "https://kubernetes.io/docs/home/learn/"},
+    {"title": "Terraform Docs – Tutorials", "desc": "Provision cloud infra with code.", "url": "https://developer.hashicorp.com/terraform/tutorials"}
+]
