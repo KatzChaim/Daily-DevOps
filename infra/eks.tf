db75a41aa63aa996738bdae2668e123c9da82c79
Katz Chaim <chaim2025@gmail.com>
2025-09-30 15:04:34 -0400

    fix bug

--- infra/eks.tf
+++ infra/eks.tf
@@ -2,14 +2,17 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.20"
 
+  # Cluster basics
   cluster_name                   = "${var.project_name}-eks"
   cluster_version                = var.eks_version
   cluster_endpoint_public_access = true
   enable_irsa                    = true
 
+  # Networking
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # Managed node group
   eks_managed_node_groups = {
     default = {
       instance_types = var.instance_types
@@ -19,6 +22,16 @@ module "eks" {
       ami_type       = "AL2_x86_64"
     }
   }
+
+  # Give your IAM user admin (kubectl) permissions inside the cluster
+  manage_aws_auth = true
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::640168441859:user/Chaim"  # your IAM user ARN
+      username = "chaim"
+      groups   = ["system:masters"]
+    }
+  ]
 
   tags = { Project = var.project_name }
 }
